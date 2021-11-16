b209c802f51d6a151bd91371917a3d896f4362a3
Vitao <vitorbc.oficial@gmail.com>
2021-11-16 10:15:06 -0300

    set styled-components interface

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color:${(props) => props.theme.colors.primary};
+`
+function Index() {
+  return <Title>Tiktok</Title>
+}
+
+export default Index;
