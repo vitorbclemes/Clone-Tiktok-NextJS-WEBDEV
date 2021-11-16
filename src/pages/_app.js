e8dd0c11dcdf0456de9967e084f74376c22d6e5f
vitorbclemes <vitorbc.oficial@gmail.com>
2021-11-16 10:37:20 -0300

    theme set-up

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,6 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
-
-const theme = {
-  colors: {
-    primary: '#fe2c55',
-  },
-}
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from '../styles/global'
+import theme from '../styles/theme'
 
 export default function App({ Component, pageProps }) {
   return (
